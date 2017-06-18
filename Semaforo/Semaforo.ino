f034d551f25641e55c478826755c56b388291c2c
labembarcado@gmail.com <labembarcado@gmail.com>
2017-06-18 19:29:29 -0300

    414 bytes, Daniel

--- Semaforo/Semaforo.ino
+++ Semaforo/Semaforo.ino
@@ -1,24 +1,36 @@
 
-void led_temp(int pin, int time);
+const long led_verde    = 30000;
+const long led_amarelo  =  5000;
+const long led_vermelho = 15000;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-}
+void mytime(unsigned int value);
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  led_temp(8, 30000);
-  led_temp(9, 5000);
-  led_temp(10,15000);
-}
-
-void led_temp(int pin, int time)
+int main() 
 {
-  digitalWrite(pin, HIGH);
-  delay(time);
-  digitalWrite(pin, LOW);
+  // put your main code here, to run repeatedly:
+
+    DDRB = 0b0000111;
+
+    while(1){
+    PORTB = 0b00000001;
+    mytime(led_verde);
+
+    PORTB = 0b00000010;
+    mytime(led_amarelo);
+  
+    PORTB = 0b00000100;
+    mytime(led_vermelho);
+    }
+
+    return 0;
+}
+
+void mytime(unsigned int value)
+{
+  unsigned long slap = 0;
+  
+  slap = millis();
+  
+  while((millis() - slap) < value);
 }
 
